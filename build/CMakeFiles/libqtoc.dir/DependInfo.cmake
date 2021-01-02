ed76608b0eb8fd52fa273519b7cc35629fe947a5
ghlian <ligu151@163.com>
2021-01-02 22:28:01 +0800

    debuging and compiling the version

---
+++ build/CMakeFiles/libqtoc.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ghlian/CODE_LIAN/code_cpp/kdb-c-interface/src/makeq.c" "/home/ghlian/CODE_LIAN/code_cpp/kdb-c-interface/build/CMakeFiles/libqtoc.dir/src/makeq.c.o"
+  "/home/ghlian/CODE_LIAN/code_cpp/kdb-c-interface/src/printq.c" "/home/ghlian/CODE_LIAN/code_cpp/kdb-c-interface/build/CMakeFiles/libqtoc.dir/src/printq.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "libqtoc_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/libqtoc.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libqtoc.dir/src/makeq.c.o"
+  "CMakeFiles/libqtoc.dir/src/printq.c.o"
+  "libqtoc.pdb"
+  "libqtoc.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/libqtoc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
